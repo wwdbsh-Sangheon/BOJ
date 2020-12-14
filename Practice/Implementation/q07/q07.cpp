ca648a222b4f0c4c9c4abf60092d3884f9d3f2c6
Sangheon Lee <wwdbsh@gmail.com>
2020-12-14 16:29:36 -0500

    q07

--- Practice/Implementation/q07/q07.cpp
+++ Practice/Implementation/q07/q07.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+string n;
+int center, l_sum, r_sum;
+
 int main(void){
+    cin >> n;
+    center = n.length()/2;
     
+    for(int i = 0; i < center; i++){
+        l_sum += (int)n[i] - '0';
+        r_sum += (int)n[center+i] - '0';
+    }
+    l_sum == r_sum ? printf("%s\n", "LUCKEY") : printf("%s\n", "READY");
 }
