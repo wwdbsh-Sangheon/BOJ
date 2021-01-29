58d1ff954a3958b0c405ddcf1f2d1a6143345866
Sangheon Lee <wwdbsh@gmail.com>
2021-01-28 20:28:46 -0500

    q26

--- Practice/Sort/q26/q26.cpp
+++ Practice/Sort/q26/q26.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
-#include <algorithm>
+#include <queue>
+#include <functional>
 
 using namespace std;
 
-int cards[100000];
+priority_queue<int, vector<int>, greater<int>> pq;
 int n;
 
 int main(void){
     cin >> n;
     for(int i = 0; i < n; i++){
-        cin >> cards[i];
+        int card;
+        cin >> card;
+        pq.push(card);
     }
-    if(n == 1){
-        return cards[0];
+    int ans = 0;
+    while(pq.size() > 1){
+        int num1 = pq.top(); pq.pop();
+        int num2 = pq.top(); pq.pop();
+        ans += (num1+num2);
+        pq.push(num1+num2);
     }
-    sort(cards, cards+n);
-    int ans = cards[0] + cards[1];
-    for(int i = 2; i < n; i++){
-        ans += (ans + cards[i]);
-    }
-    cout << ans << "\n";
+    cout << ans << "\n"; pq.pop();
 }
