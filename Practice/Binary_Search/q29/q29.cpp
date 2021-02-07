af73f9a0943ef4b507b96bebf9a5d7e029babc9e
Sangheon Lee <wwdbsh@gmail.com>
2021-02-07 18:05:48 -0500

    20210207:q29_update

--- Practice/Binary_Search/q29/q29.cpp
+++ Practice/Binary_Search/q29/q29.cpp
@@ -4,7 +4,28 @@
 using namespace std;
 
 int homes[200000];
-int n, c;
+int n, c, dist;
+
+void get_dist(int start, int end){
+    if(start > end){
+        return;
+    }
+    int mid = (start + end) / 2;
+    int value = homes[0];
+    int count = 1;
+    for(int i = 1; i < n; i++){
+        if(homes[i] >= value + mid){
+            value = homes[i];
+            count++;
+        }
+    }
+    if(count >= c){
+        dist = mid;
+        get_dist(mid+1, end);
+    }else{
+        get_dist(start, mid-1);
+    }
+}
 
 int main(void){
     cin >> n >> c;
@@ -12,5 +33,8 @@ int main(void){
         cin >> homes[i];
     }
     sort(homes, homes+n);
-    
+    int start = 1;
+    int end = homes[n-1] - homes[0];
+    get_dist(start, end);
+    cout << dist << "\n";
 }
